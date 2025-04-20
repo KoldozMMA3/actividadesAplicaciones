e1788ebfe22fa9bc3275b73072d257af976995da
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2025-04-20 02:20:40 -0500

    Al presionar el botón compartir, la nota se envia a otra actividad

--- PRACTICA 4/Caso2_EditorNota/app/src/main/java/com/example/practica4casob/EditorActivity.kt
+++ PRACTICA 4/Caso2_EditorNota/app/src/main/java/com/example/practica4casob/EditorActivity.kt
@@ -1,20 +1,48 @@
 package com.example.practica4casob
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class EditorActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNota: EditText
+    private var notaTexto = ""
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            notaTexto = data?.getStringExtra("nota") ?: ""
+            etNota.setText(notaTexto)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etNota = findViewById(R.id.etNota)
+        val btnCompartir = findViewById<Button>(R.id.btnCompartir)
+
+        if (savedInstanceState != null) {
+            notaTexto = savedInstanceState.getString("nota", "")
+            etNota.setText(notaTexto)
         }
+
+        btnCompartir.setOnClickListener {
+            val texto = etNota.text.toString()
+            val intent = Intent(this, OpcionesActivity::class.java)
+            intent.putExtra("nota", texto)
+            launcher.launch(intent)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("nota", etNota.text.toString())
     }
 }
