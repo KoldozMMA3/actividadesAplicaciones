79e60c080b34980f114bc8e50bfaab7d69992b25
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2025-04-24 14:11:02 -0500

    Ahora agregue la parte de resumen en la que realizara la operacion de guardar la informacion que los clic que realiza el usuario

--- PRACTICA 5/app/src/main/java/com/example/navegacion/ResumenPedidoFragment.kt
+++ PRACTICA 5/app/src/main/java/com/example/navegacion/ResumenPedidoFragment.kt
@@ -1,2 +1,38 @@
 package com.example.navegacion
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+
+
+class ResumenPedidoFragment : Fragment(R.layout.fragment_resumen_pedido) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tipoComida = arguments?.getString("tipo_comida")
+        val extras = arguments?.getString("extras_comida")
+
+
+        val btnConfirmar: Button = view.findViewById(R.id.btnConfirmar)
+        btnConfirmar.setOnClickListener {
+            Toast.makeText(requireContext(), "Pedido confirmado: $tipoComida con $extras", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_resumenPedidoFragment_to_inicioFragment)
+        }
+
+        val btnEditar: Button = view.findViewById(R.id.btnEditar)
+        btnEditar.setOnClickListener {
+            val result = Bundle().apply {
+                putString("tipo_comida", tipoComida)
+                putString("extras", extras)
+            }
+            setFragmentResult("requestKey", result)
+            findNavController().popBackStack()
+        }
+    }
+}
