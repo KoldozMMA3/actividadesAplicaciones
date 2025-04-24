85bd10e0364b2a7322e69423f1d1555e42ef58cc
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2025-04-24 14:07:50 -0500

    enlazo mi clase inicio con mi fragment_inicio para el procedimiento de inicio

--- PRACTICA 5/app/src/main/java/com/example/navegacion/InicioFragment.kt
+++ PRACTICA 5/app/src/main/java/com/example/navegacion/InicioFragment.kt
@@ -1,2 +1,19 @@
 package com.example.navegacion
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+
+class InicioFragment : Fragment(R.layout.fragment_inicio) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnNuevoPedido: Button = view.findViewById(R.id.btnNuevoPedido)
+        btnNuevoPedido.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_seleccionComidaFragment)
+        }
+    }
+}
