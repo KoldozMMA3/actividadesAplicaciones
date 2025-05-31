af200016b4f82f554b8a2cb38afdf2a6e31685c7
CristianLuna <ccarrizalesl@ulasalle.edu.pe>
2025-05-31 02:26:22 -0500

    Realizo la implementacion de la clase main para que pueda utilizar la clase pantalla inicio

---
+++ Practica1_Flutter/lib/pantalla_inicio.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PantallaInicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bienvenido'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '¡Hola! Bienvenido a mi app personal.',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Ver mi perfil'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
